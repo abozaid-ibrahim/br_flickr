9459c32c052281fcadb972d207563678dc695fc2
Abubakar Oladeji <3057485+tonespy@users.noreply.github.com>
2019-11-05 22:56:30 +0100

    App Structure
    * Added mock for handling request manager

--- br_flickr/Network/RequestFactory.swift
+++ br_flickr/Network/RequestFactory.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+struct RequestFactory {
+    
+    enum Method: String {
+        case GET
+        case POST
+        case PUT
+        case DELETE
+        case PATCH
+    }
+    
+    static func request(method: Method, url: URL) -> URLRequest {
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        return request
+    }
+}
--- br_flickr/Network/RequestManager.swift
+++ br_flickr/Network/RequestManager.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+
+public enum BRError: Error {
+    
+    case network(string: String)
+    case custom(string: String)
+    
+    var localizedDescription: String {
+        switch self {
+            case .network(let value):
+                return value
+            case .custom(let value):
+                return value
+        }
+    }
+}
+
+open class RequestManager: NSObject {
+    
+    public typealias RequestCompletion = (Result<Data, BRError>) -> ()
+    
+    func handleGETRequest(urlString: String, session: URLSession = URLSession(configuration: .default), completion: @escaping RequestCompletion) -> URLSessionTask? {
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.network(string: "Wrong url format")))
+            return nil
+        }
+        
+        var request = RequestFactory.request(method: .GET, url: url)
+        
+        if let reachability = try? Reachability(), reachability.connection != .unavailable {
+            request.cachePolicy = .returnCacheDataDontLoad
+        }
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completion(.failure(.network(string: "\(error.localizedDescription), please try again later.")))
+                return
+            }
+            
+            if let data = data {
+                completion(.success(data))
+            }
+        }
+        task.resume()
+        return task
+    }
+}
